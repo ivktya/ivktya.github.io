f554c41f18cbd95774ea9df46274cf9f70aac590
ivktya <32465921+ivktya@users.noreply.github.com>
2026-05-27 13:15:26 +0200

    moved to the left

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const boxes = document.querySelectorAll('.box');
 
     tabs.forEach(tab => {
+
         tab.addEventListener('click', () => {
+            
             const i = parseInt(tab.dataset.index);
             
             boxes.forEach((b, j) => b.classList.toggle('hidden', j !== i));
